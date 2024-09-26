f66c474dd84ac70509888a4314a87316c5f34986
levismim <86032562+levismim@users.noreply.github.com>
2024-09-25 20:10:10 -0500

    create HTML and write algorithm for script

---
+++ script.js
@@ -0,0 +1,7 @@
+//  create string variables for rock paper and scissors.
+//  return random choice from computer
+//  get user's choice
+//  create and initialize variables for scores in global scope
+//  play round using computer's choice and user's choice as arguments (make case insensitive)
+//  output string value representing winner
+// increment each score variable based on winner
