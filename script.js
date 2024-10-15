fc74322d8ba80fd86a3e65f5af3e4ce5063e17ee
levismim <86032562+levismim@users.noreply.github.com>
2024-10-15 17:08:18 -0500

    make buttons and scores visible on page

--- script.js
+++ script.js
@@ -1,78 +1,104 @@
-//  return random integer from computer
+
 function getCompChoice() {
     const computerChoice = Math.floor(Math.random() * 3);
-//convert random integer into strings
-    switch (computerChoice) {
-        case 0:
-            return "rock";
-        case 1:
-            return "paper";
-        case 2:
-            return "scissors";
+        switch (computerChoice) {
+            case 0:
+                return "rock";
+            case 1:
+                return "paper";
+            case 2:
+                return "scissors";
 }  
 }
-//  get user's choice
 
-let buttons = document.getElementById("buttons")
-    let rock = document.createElement("button")
-    let paper = document.createElement("button")
-    let scissors = document.createElement("button")
-    buttons.appendChild(rock)
-    buttons.appendChild(paper)
-    buttons.appendChild(scissors)
     
+    const scoreBoard = document.createElement("div");
+    const finalHScore = document.createElement("p");
+    const finalCScore = document.createElement("p");
+    const winner = document.createElement("p");
+
+        document.body.appendChild(scoreBoard);
+            scoreBoard.appendChild(finalHScore);
+            scoreBoard.appendChild(finalCScore);
+            scoreBoard.appendChild(winner);    
+                finalHScore.id = "hScoreID";
+                finalCScore.id = "cScoreID";
+                winner.id = "winnerID";
+
+
+    const buttons = document.querySelectorAll("button")
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            let humanChoice = button.id;
+            let computerChoice = getCompChoice();
+
+            playRound(humanChoice, computerChoice);
+                while (computerScore === 5 || humanScore === 5){
+                    if (computerScore > humanScore) {
+                        document.getElementById("winnerID").textContent = "Winner: Computer!"
+                        console.log("You lost to a computer!")
+                    
+                } 
+                else if (computerScore < humanScore) {
+                    document.getElementById("winnerID").textContent = "Winner: Human!"
+                    console.log("You won against a computer!")
+                    break
+                }
+                else if (computerScore === humanScore){
+                    document.getElementById("winnerID").textContent = "Winner: Nobody lol"
+                    console.log("It's a tie!")
+                    break;
+                }                
+            break;            
+            }
+        });    
+    });
+    
+
+      
 
 //  create and initialize variables for scores in global scope
 let humanScore = 0;
 let computerScore = 0;  
+document.getElementById("hScoreID").textContent = "Your Score: " + humanScore;
+document.getElementById("cScoreID").textContent = "The Computer's Score: " + computerScore;
 //  play round using computer's choice and user's choice as arguments (make case insensitive)
-function playRound() {
-const humanChoice = getHumanChoice()
-const computerChoice = getCompChoice()
-  
-if (humanChoice == computerChoice) {
-        console.log("DRAW!")
+function playRound(humanChoice, computerChoice) {
+        if (humanChoice == computerChoice) {
+            document.getElementById("hScoreID").textContent = "Draw! Your Score: " + humanScore;
+            document.getElementById("cScoreID").textContent = "The Computer's Score: " + computerScore;
         }
-    else if (humanChoice == "rock" && computerChoice == "paper") {
-        console.log("You lose! Paper beats rock!")
-        computerScore++
+        else if (humanChoice == "rock" && computerChoice == "paper") {
+            computerScore++
+            document.getElementById("hScoreID").textContent = "Uh oh! Paper beats rock. Your Score: " + humanScore;
+            document.getElementById("cScoreID").textContent = "The Computer's Score: " + computerScore;
+        
+        }
+        else if (humanChoice == "rock" && computerChoice == "scissors"){
+            humanScore++
+            document.getElementById("hScoreID").textContent = "Yay! Rock beats scissors. Your Score: " + humanScore;
+            document.getElementById("cScoreID").textContent = "The Computer's Score: " + computerScore;
+        }
+        else if (humanChoice == "paper" && computerChoice == "rock") {
+            humanScore++
+            document.getElementById("hScoreID").textContent = "Yay! Paper beats rock! Your Score: " + humanScore;
+            document.getElementById("cScoreID").textContent = "The Computer's Score: " + computerScore;
+        
+        }
+        else if (humanChoice == "paper" && computerChoice == "scissors") {
+            computerScore++
+            document.getElementById("hScoreID").textContent = "Uh oh! Scissors beat paper. Your Score: " + humanScore;
+            document.getElementById("cScoreID").textContent = "The Computer's Score: " + computerScore;
+        
+        }
+        else if (humanChoice == "scissors" && computerChoice == "paper") {
+            humanScore++
+            document.getElementById("hScoreID").textContent = "Yay! Scissors beat paper. Your Score: " + humanScore;
+            document.getElementById("cScoreID").textContent = "The Computer's Score: " + computerScore;    
+        }
+        else if (humanChoice == "scissors" && computerChoice == "rock") {
+         computerScore++
+         document.getElementById("hScoreID").textContent = "Uh oh. Rock beats scissors. Your Score: " + humanScore;
+        document.getElementById("cScoreID").textContent = "The Computer's Score: " + computerScore;   
+        }                
     }
-    else if (humanChoice == "rock" && computerChoice == "scissors"){
-        console.log("You win! Rock beats scissors!")
-        humanScore++
-    }
-    else if (humanChoice == "paper" && computerChoice == "rock") {
-        console.log("You win! Paper beats rock!")
-        humanScore++
-    }
-     else if (humanChoice == "paper" && computerChoice == "scissors") {
-        console.log("You lose! Scissors beats paper!")
-        computerScore++
-     }
-    else if (humanChoice == "scissors" && computerChoice == "paper") {
-        console.log("You win! Scissors beats paper!")
-        humanScore++
-    }
-    else if (humanChoice == "scissors" && computerChoice == "rock") {
-        console.log("You lose!")
-        computerScore++
-    }                
-    console.log(humanScore, computerScore)
-}
-
-function playGame() {
-    if (computerScore > humanScore) {
-        console.log("You lost to a computer!")
-} 
-    else if (computerScore < humanScore) {
-        console.log("You won against a computer!")
-}
-    else if (computerScore === humanScore){
-        console.log("It's a tie!")
-}
-    
-
-}
-
-//  output string value representing winner
-// increment each score variable based on winner
